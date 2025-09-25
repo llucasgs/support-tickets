bc35ac7c6419e40747f2a952e9200fdc6166ee84
Lucas Garcia e Silva <llucasgarciaesilva@hotmail.com>
2025-09-25 19:57:04 -0300

    configurando uma justificativa para fechar um ticket e configurando rota para remoção

--- src/controllers/tickets/updateStatus.js
+++ src/controllers/tickets/updateStatus.js
@@ -1,6 +1,7 @@
 export function updateStatus({ request, response, database }) {
   const { id } = request.params;
+  const { solution } = request.body;
 
-  database.update("tickets", id, { status: "closed" });
+  database.update("tickets", id, { status: "closed", solution });
   return response.end();
 }
